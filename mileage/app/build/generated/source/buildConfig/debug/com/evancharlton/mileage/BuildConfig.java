514d80fad7895b44198693d6139b8cffc86b1b71
Camilo Andrés Escobar Velásquez <ca.escobar2434@uniandes.edu.co>
2018-11-19 15:18:16 -0500

    All apps

---
+++ mileage/app/build/generated/source/buildConfig/debug/com/evancharlton/mileage/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.evancharlton.mileage;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.evancharlton.mileage";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 3110;
+  public static final String VERSION_NAME = "3.1.1";
+}
